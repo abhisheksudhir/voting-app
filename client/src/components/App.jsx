a358a0e029d716a990164c6421b90cb56f072d95
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-03-07 20:14:16 +0530

    made api service and changed proxy from package.json to api

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import api from '../services/api'
 
-const App = () => <div>APP WORKS</div>;
+// const App = () => <div>APP WORKS</div>;
+
+class App extends React.Component{
+    async componentDidMount() {   ////runs imediately after the render method. Is a lifecycle method
+        const result = await api.call('post','auth/login', {
+            "username":"username",
+            "password":"password"
+        });
+        console.log(result);
+    }
+    
+    render(){
+        return <div>APP WORKS</div>;
+    }
+}
 
 export default App;
 
